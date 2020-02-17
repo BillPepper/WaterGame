6189b08717e86f05c8812accbf7dbb1c664d92da
Mattes Wenzel <mattes@artfacts.net>
2020-02-17 16:03:52 +0100

    add an ememy counter

---
+++ Assets/DbgEnemyCount.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DbgEnemyCount : MonoBehaviour
+{
+    private Text ownText;
+
+    private void Start()
+    {
+        this.ownText = GetComponent<Text>();
+        this.setText(-1);
+    }
+    void Update()
+    {
+        this.setText(getEnemyCount());
+    }
+
+    int getEnemyCount()
+    {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+
+        return enemies.Length;
+    }
+
+    void setText(int c)
+    {
+        this.ownText.text = "EnemyCount: " + c;
+    }
+}
