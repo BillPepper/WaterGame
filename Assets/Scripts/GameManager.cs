6a4d63b6692848dc02c5be525fcb6720a39a1eee
Mattes Wenzel <mattes@artfacts.net>
2020-02-17 12:45:04 +0100

    make player start walking on keypress

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public PlayerController Player;
+
+    private bool gameIsStarted;
+    private bool gameIsLost;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)){
+            this.gameIsStarted = true;
+            Player.SetIsWalking(true);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 2f;
+
+    private bool isWalking = false;
+
+    public void FixedUpdate()
+    {
+        if (this.isWalking)
+        {
+            this.transform.position = new Vector2(this.transform.position.x + speed * Time.fixedDeltaTime, this.transform.position.y);
+        }
+    }
+
+    public bool SetIsWalking()
+    {
+        return this.isWalking;
+    }
+
+    public void SetIsWalking(bool state)
+    {
+        this.isWalking = state;
+    }
+}
