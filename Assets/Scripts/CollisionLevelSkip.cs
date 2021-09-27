d88d1a2bbe06a63470a1dba2276addce04c1f2d3
Mattes Wenzel <mattes@artfacts.net>
2021-09-27 19:57:53 +0200

    add level skip script

---
+++ Assets/Scripts/CollisionLevelSkip.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class CollisionLevelSkip : MonoBehaviour
+{ 
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        string collisionObjTag = collision.gameObject.tag;
+        if (collisionObjTag == "LevelEnd")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
