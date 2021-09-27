f818b8ac0c18140c483b6d2b52388035f7f24a89
Mattes Wenzel <mattes@artfacts.net>
2021-09-27 19:58:41 +0200

    add keyAnimation script

---
+++ Assets/Scripts/KeyAnimation.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KeyAnimation: MonoBehaviour
+{
+
+    public Animator animator;
+    public KeyCode inputKey;
+    public string animationName;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(inputKey))
+        {
+            animator.Play(animationName);
+        }
+    }
+}
+
