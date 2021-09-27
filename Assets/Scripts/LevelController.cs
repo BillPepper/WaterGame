6d50f744d2f36c96dead5e7011ad3d7ad7c71062
Mattes Wenzel <mattes@artfacts.net>
2021-09-27 19:58:25 +0200

    add base level controller

---
+++ Assets/Scripts/LevelController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+// Unfinished!!!
+public class LevelController : MonoBehaviour
+{
+    void Update()
+    {
+        KeyCode[] keys = new KeyCode[] {
+            KeyCode.Alpha0,
+            KeyCode.Alpha1,
+            KeyCode.Alpha2,
+            KeyCode.Alpha3,
+            KeyCode.Alpha4,
+            KeyCode.Alpha5,
+            KeyCode.Alpha6,
+            KeyCode.Alpha7,
+            KeyCode.Alpha8,
+            KeyCode.Alpha9
+        };
+
+        foreach (KeyCode key in keys)
+        {  
+
+        }
+    }
+
+    void SwitchScene(int id)
+    {
+        Debug.Log("Switching scene: " + id);
+        SceneManager.LoadScene(id);
+    }
+}
